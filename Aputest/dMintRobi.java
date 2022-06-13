617801962f79266bbb082d2367de2cdb0773c6d6
Horváth Róbert <croroby@gmail.com>
2022-06-13 11:14:12 +0200

    d2

--- Aputest/dMintRobi.java
+++ Aputest/dMintRobi.java
@@ -1,5 +1,10 @@
 public class dMintRobi {
     public static void main(String[] args) {
+        future();
+    }
+
+    public static void future(){
         System.out.println("Cancer");
     }
+
 }
