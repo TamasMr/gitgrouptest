23585fb421e80469757178f411bb17f48620f297
Molnar Tamas <tamas.molnar.801001@gmail.com>
2022-06-13 13:26:34 +0200

    a5

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Dtest.future("Water Rooster");
-        Atest.greet("Tamás");
+        Atest.greet("Tamás",41);
         CTest.cheer("Java");
         Btest.count(10);
         
