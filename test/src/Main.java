29c74da1827d881722994c2f9fe75206c1ae6638
Rumy Balázs <balazs778@gmail.com>
2022-06-13 13:44:26 +0200

    conflict resolved

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,17 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Dtest.future("Water Rooster","Cancer");
-        Atest.greet("Tamás");
         CTest.cheer("Java");
-        Btest.count(10);
-
-=======
-        Dtest.future("Water Rooster");
         Atest.greet("Tamás",41);
-        CTest.cheer("Java");
         Btest.count("Balazs",10);
-        
->>>>>>> e9807d81405a4a4f072bcd448852401a4d383882
     }
 }
